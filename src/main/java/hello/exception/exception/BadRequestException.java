8ef18b49b1e40cc00692ce17497a1bbd1d3e0f43
Newbiedevelope <rbtjr20313@gmail.com>
2023-02-22 03:09:50 +0900

    first commit

---
+++ src/main/java/hello/exception/exception/BadRequestException.java
@@ -0,0 +1,9 @@
+package hello.exception.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+//@ResponseStatus(code = HttpStatus.BAD_REQUEST, reason = "잘못된 요청 오류")
+@ResponseStatus(code = HttpStatus.BAD_REQUEST, reason = "error.bad")
+public class BadRequestException extends RuntimeException{
+}
